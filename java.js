9257f6ca73f23f4aa54f6bfb54e1222e8998c11b
maysaahourani <maysaahourani@gmail.com>
2021-02-19 13:03:50 +0200

    lab1

---
+++ java.js
@@ -0,0 +1,23 @@
+let input1=prompt('what is yourname?');
+console.log(input1);
+alert('hello '+input1+' welcome to my website');
+
+let input2=prompt('how old are you');
+console.log(input2);
+alert('hso you are '+input2+' yesrs old and i am 28 years old');
+
+
+let input3=prompt('what is your major?');
+console.log(input3);
+alert(input3+' is a good major to study');
+
+
+let input4=prompt('do you love programming');
+console.log(input4);
+if(input4==='yes'){
+alert('yes i love programming too ');
+}else{
+    alert('why you dont try it again ');
+    }
+
+
